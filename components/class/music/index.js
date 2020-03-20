3a4eab4719c8cd11ac52cf867aabe897a6dea756
yysfwls@163.com <yysfwls@163.com>
2020-03-20 22:46:53 +0800

    music

--- components/class/music/index.js
+++ components/class/music/index.js
@@ -1,22 +1,46 @@
-import { classicBeh } from '../classic-beh.js'
+import {
+  classicBeh
+} from '../classic-beh.js'
+
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   behaviors: [classicBeh],
   properties: {
-
+    src: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    pauseSrc: 'images/player@waitting.png',
-    playSrc: 'images/player@playing.png'
+    playing: false,
+    pauseSrc: 'images/player@pause.png',
+    playSrc: 'images/player@play.png'
+  },
+  attached:function(){
+
+  },
+  detached: function(event){
+    // mMgr.stop()
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPlay: function() {
+      if (!this.data.playing) {
+        this.setData({
+          playing: true
+        })
+        mMgr.src = this.properties.src
+      } else{
+        this.setData({
+          playing: false
+        })
+        mMgr.pause()
+      }
+    }
   }
 })
