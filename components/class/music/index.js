34dd26984f22e906b16595a8de6758f44dbe7edd
yysfwls@163.com <yysfwls@163.com>
2020-04-15 14:44:54 +0800

    注释

--- components/class/music/index.js
+++ components/class/music/index.js
@@ -2,7 +2,8 @@ import {
   classicBeh
 } from '../classic-beh.js'
 
-const mMgr = wx.getBackgroundAudioManager()
+const mMgr = wx.getBackgroundAudioManager()  
+// 背景音乐方法
 
 Component({
   behaviors: [classicBeh],
@@ -16,20 +17,22 @@ Component({
   data: {
     playing: false,
     pauseSrc: 'images/player@pause.png',
-    playSrc: 'images/player@play.png'
+    playSrc: 'images/player@play.png',
   },
-  attached:function(){
+  attached:function(event){ 
+    // 每次页面时判断播放图标的样式
 
   },
   detached: function(event){
-    // mMgr.stop()
+    // mMgr.stop() 
+    // 没用   page hidden不会触发detach
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    onPlay: function() {
+    onPlay: function(event) {
       if (!this.data.playing) {
         this.setData({
           playing: true
--- models/classic.js
+++ models/classic.js
@@ -14,19 +14,24 @@ class ClassicModel extends HTTP {
       }
     })
   }
-  //先去缓存中取 没有的话调api
+  //getClassic这个方法是寻找index期刊的上一期还是下一期
   getClassic(index, nextOrPrevious, sCallback) {
-    let key = nextOrPrevious == 'next' ? this._getKey(index + 1) : this._getKey(index - 1)
+    // 缓存中寻找 or API 写入到缓存中
+    // key 确定key
+    let key = nextOrPrevious == 'next' ?
+      this._getKey(index + 1) : this._getKey(index - 1)
     let classic = wx.getStorageSync(key)
+    // 去缓存中寻找
     if (!classic) {
       this.request({
         url: `classic/${index}/${nextOrPrevious}`,
         success: (res) => {
-          wx.setStorageSync(this._getKey(res.index), res)
+          wx.setStorageSync(
+            this._getKey(res.index), res)
           sCallback(res)
         }
       })
-    } else{
+    } else {
       sCallback(classic)
     }
   }
@@ -45,7 +50,7 @@ class ClassicModel extends HTTP {
     let index = wx.getStorageSync('latest')
     return index;
   }
-  //  确定key 既能表示那个期刊
+  //  确定key 私有的方法_开头，既能表示那个期刊，还能表示是哪期的期刊
   _getKey(index) {
     let key = 'classic-' + index
     return key;
