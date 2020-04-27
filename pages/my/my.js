bcce1c1ea56b26587dec342b40079bfd52a84462
yysfwls@163.com <yysfwls@163.com>
2020-04-27 18:54:08 +0800

    my

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,86 @@
 // pages/my/my.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    authorized: false,
+    userInfo: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.userAuthorized();
+    // wx.getUserInfo({
+    //   success: (data) => {
+    //     console.log(data);
+    //   },
+    // });
+  },
 
+  //是否授权
+  userAuthorized() {
+    wx.getSetting({
+      success: (data) => {
+        if (data.authSetting["scope.userInfo"]) {
+          wx.getUserInfo({
+            success: (data) => {
+              this.setData({
+                authorized: true,
+                userInfo: data.userInfo,
+              });
+            },
+          });
+        } else {
+          console.log("err");
+        }
+      },
+    });
+  },
+  onGetUserInfo(event) {
+    let userInfo = event.detail.userInfo;
+    if (userInfo) {
+      this.setData({
+        authorized: true,
+        userInfo,
+      });
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
